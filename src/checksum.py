ff8b95b7ee63d664b0fa3309393b3a56d677d57b
Alexander Grant <alexander.grant@dunelm.com>
2022-12-06 23:58:55 +0000

    Refactor code, expand test suite, test execute still WIP.

--- src/checksum.py
+++ src/checksum.py
@@ -1,39 +1,72 @@
 """Doc."""
-
 from typing import List
 
-with open("input.txt") as f:
-    lines = f.read().splitlines()
+
+def open_file(filepath) -> List:
+    """Open a file and return a list of the lines in the file."""
+    try:
+        with open(filepath) as f:
+            box_ids = [line.strip() for line in f]
+    except FileNotFoundError:
+        raise FileNotFoundError("File not found")
+
+    return box_ids
 
 
-def char_frequency(box_id):
-    return {
-        character: box_id.count(character) for character in box_id
-    }
+def char_frequency(box_id: str) -> dict:
+    """Create a dictionary of the frequency of each character in a box id."""
+    frequency_dict = {}
+    # check if box_id is a string
+    if not isinstance(box_id, str):
+        raise TypeError("box_id must be a string")
+
+    # check if the box id is empty
+    if not box_id:
+        raise ValueError("box_id must not be empty")
+
+    # check if the box id contains spaces
+    if " " in box_id:
+        raise ValueError("box_id must not contain spaces")
+
+    for char in box_id:
+        frequency_dict[char] = frequency_dict.get(char, 0) + 1
+    return frequency_dict
 
 
-def has_pair(char_counts):
-    return 2 in char_counts.values()
+def count_occurrences(frequency_dict: dict, id) -> tuple:
+    """Count the number of pairs and triplets in a box id."""
+    # Use a set to keep track of the box IDs that contain
+    # a pair or a triplet.
+    pairs = set()
+    triplets = set()
+
+    # Use the any() function to check if any elements
+    # of the char_frequency_dict
+    # match the target values of 2 and 3.
+    # If so, add the box ID to the
+    #  corresponding set.
+    if any(x == 2 for x in frequency_dict.values()):
+        pairs.add(id)
+    if any(freq == 3 for freq in frequency_dict.values()):
+        triplets.add(id)
+
+    return pairs, triplets
 
 
-def has_triplets(char_counts):
-    return 3 in char_counts.values()
+def execute(filepath) -> int:
+    """Execute the checksum algorithm."""
+    count_pairs = 0
+    count_triplets = 0
 
-
-def box_checksum_calculator(box_ids: List) -> int:
-    """Check for unique occurrences of pairs and triplets in box ids."""
-    count_of_pairs = 0
-    count_of_triplets = 0
+    box_ids = open_file(filepath)
 
     for id in box_ids:
-        character_count_dict = char_frequency(id)
+        frequency_dict = char_frequency(id)
+        pairs, triplets = count_occurrences(frequency_dict, id)
+        count_pairs += len(pairs)
+        count_triplets += len(triplets)
 
-        if has_pair(character_count_dict):
-            count_of_pairs += 1
-        if has_triplets(character_count_dict):
-            count_of_triplets += 1
-
-    return count_of_pairs * count_of_triplets
+    return count_pairs * count_triplets
 
 
-# print(count_characters(box_ids))
+execute("input.txt")
--- tests/unit_tests/test_checksum.py
+++ tests/unit_tests/test_checksum.py
@@ -8,11 +8,18 @@
     ababab contains three a and three b, but it only counts once.
 """
 
-from src.checksum import box_checksum_calculator, char_frequency, has_pair, has_triplets
+
+# from unittest.mock import call, patch
+
+import pytest
+
+from src.checksum import char_frequency, count_occurrences, open_file
 
 
-def test_input_is_list():
-    expected_return = [
+def test_open_file():
+    filepath = "tests/unit_tests/box_ids.txt"
+    box_ids = open_file(filepath)
+    assert box_ids == [
         "abcdef",
         "bababc",
         "abbcde",
@@ -22,39 +29,113 @@ def test_input_is_list():
         "ababab",
     ]
 
-    with open("tests/unit_tests/mock_input.txt") as f:
-        lines = f.read().splitlines()
+    # Test with a file that does not exists
+    filepath = "does_not_exist.txt"
+    with pytest.raises(FileNotFoundError):
+        open_file(filepath)
 
-        assert lines == expected_return
-
-
-def test_character_frequency():
-    expected_return = {"a": 1, "b": 1, "c": 1, "d": 1, "e": 1, "f": 1}
-
-    assert char_frequency("abcdef") == expected_return
-
-
-def test_has_pair():
-    char_counts = {"a": 1, "b": 2, "c": 1, "d": 1, "e": 1}
-    assert has_pair(char_counts) is True
-    # To do: Add more tests, eg edge cases and False
+    # Test with an empty file
+    filepath = "tests/unit_tests/empty.txt"
+    with open(filepath, "w") as f:
+        f.write("")
+    box_ids = open_file(filepath)
+    assert box_ids == []
 
 
-def test_has_triplets():
-    char_counts = {"a": 1, "b": 1, "c": 3, "d": 1}
-    assert has_triplets(char_counts) is True
-    # To do: Add more tests, eg edge cases and False
+def test_char_frequency():
+
+    # Test with a valid box ID
+    box_id = "abcdef"
+    frequency_dict = char_frequency(box_id)
+    assert frequency_dict == {"a": 1, "b": 1, "c": 1, "d": 1, "e": 1, "f": 1}
+
+    # Test with a box ID that contains a repeated characters
+    box_id = "aabbcc"
+    frequency_dict = char_frequency(box_id)
+    assert frequency_dict == {"a": 2, "b": 2, "c": 2}
+
+    # Test with a box ID that contains all the same characters
+    box_id = "aaaaaa"
+    frequency_dict = char_frequency(box_id)
+    assert frequency_dict == {"a": 6}
+
+    # Test with a box ID that contains no characters
+    box_id = ""
+    with pytest.raises(ValueError):
+        char_frequency(box_id)
+
+    # Test with a box ID that contains spaces
+    box_id = "a b c"
+    with pytest.raises(ValueError):
+        char_frequency(box_id)
+
+    # Test with a box ID that is not a string
+    box_id = 123
+    with pytest.raises(TypeError):
+        char_frequency(box_id)
 
 
-def test_box_checksum_calculator():
-    mock_box_ids = [
-        "abcdef",
-        "bababc",
-        "abbcde",
-        "abcccd",
-        "aabcdd",
-        "abcdee",
-        "ababab",
-    ]
+def test_char_frequency_raises_type_error():
+    with pytest.raises(TypeError):
+        char_frequency(123)
 
-    assert box_checksum_calculator(mock_box_ids) == 12
+    with pytest.raises(TypeError):
+        char_frequency(["a", "b", "c"])
+
+
+def test_count_occurrences():
+
+    # Test with a box ID that contains a pair
+    frequency_dict = {"a": 2, "b": 1, "c": 1}
+    id = "abac"
+    pairs, triplets = count_occurrences(frequency_dict, id)
+
+    assert pairs == {"abac"}
+    assert triplets == set()
+
+    # Test with a box ID that contains a triplet
+    frequency_dict = {"a": 3, "b": 1}
+    id = "aaab"
+    pairs, triplets = count_occurrences(frequency_dict, id)
+
+    assert pairs == set()
+    assert triplets == {"aaab"}
+
+    # Test with a box ID that contains a pair and a triplet
+    frequency_dict = {"a": 2, "b": 3}
+    id = "aabbb"
+    pairs, triplets = count_occurrences(frequency_dict, id)
+
+    assert pairs == {"aabbb"}
+    assert triplets == {"aabbb"}
+
+    # Test with a box ID that contains no pairs or triplets
+    frequency_dict = {"a": 1, "b": 1, "c": 1}
+    id = "abc"
+    pairs, triplets = count_occurrences(frequency_dict, id)
+
+    assert pairs == set()
+    assert triplets == set()
+
+
+# @patch("src.checksum.open_file")
+# @patch("src.checksum.char_frequency")
+# @patch("src.checksum.count_occurrences")
+# def test_execute(mock_count_occurrences, mock_char_frequency,
+# mock_open_file):
+
+#     mock_open_file.return_value = [
+#         "abcdef",
+#         "bababc",
+#         "abbcde",
+#         "abcccd",
+#         "aabcdd",
+#         "abcdee",
+#         "ababab",
+#     ]
+
+#     mock_char_frequency.return_value = {"a": 2, "b": 2, "c": 2}
+
+#     mock_count_occurrences.return_value = {(2, 3), "aabbcc"}
+
+#     assert execute("tests/unit_tests/box_ids.txt") == 12
