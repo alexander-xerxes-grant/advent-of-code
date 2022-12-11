b89f2043f9a3649ee763767f48f87720e54ed148
Alexander Grant <alexander.grant@dunelm.com>
2022-12-11 19:36:59 +0000

    Add tests for check_outcome() function.

--- day02/test_solution1.py
+++ day02/test_solution1.py
@@ -1,4 +1,4 @@
-from day02.solution1 import map_moves, parse_input
+from day02.solution1 import check_outcome, map_moves, parse_input
 
 
 def test_parser():
@@ -9,3 +9,15 @@ def test_parser():
 def test_map_moves():
     assert map_moves("A X, B Y, C Z") == [(0, 0), (1, 1), (2, 2)]
     assert map_moves("B Y, C X, C Z") == [(1, 1), (2, 0), (2, 2)]
+
+
+def test_check_outcome():
+    assert check_outcome(0, 0) == 3
+    assert check_outcome(1, 1) == 3
+    assert check_outcome(2, 2) == 3
+    assert check_outcome(0, 1) == 0
+    assert check_outcome(1, 2) == 0
+    assert check_outcome(2, 0) == 0
+    assert check_outcome(0, 2) == 6
+    assert check_outcome(1, 0) == 6
+    assert check_outcome(2, 1) == 6
