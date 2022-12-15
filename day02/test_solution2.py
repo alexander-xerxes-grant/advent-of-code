96c15e5ed458f6ade2f1fb35d3acf8fe72a6e9a5
Alexander Grant <alexander.grant@dunelm.com>
2022-12-15 22:20:41 +0000

    Add tests for player_move functionality.

--- day02/test_solution2.py
+++ day02/test_solution2.py
@@ -1,8 +1,9 @@
-from day02.solution2 import calculate
+from day02.solution2 import player_move
 
 
 # def test_calculate():
 #     assert calculate("A Y") == 4
 
-def player_move(opponent, outcome):
-    
+
+def test_player_move():
+    assert player_move("A Y") == "X"
