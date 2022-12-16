9155785a09821cfd2d967308d890494368909d72
Alexander Grant <alexander.grant@dunelm.com>
2022-12-16 07:51:08 +0000

    Add tests for parse_input.

--- day02/test_solution2.py
+++ day02/test_solution2.py
@@ -1,9 +1,17 @@
-from day02.solution2 import player_move
+from day02.solution2 import parse_input
 
 
 # def test_calculate():
 #     assert calculate("A Y") == 4
 
 
-def test_player_move():
-    assert player_move("A Y") == "X"
+# def test_player_move():
+#     assert player_move("A Y") == "X"
+#     assert player_move("B X") == "X"
+#     assert player_move("C Z") == "X"
+
+
+def test_parse_input():
+    assert parse_input("A X") == (0, -1)
+    assert parse_input("B Y") == (1, 0)
+    assert parse_input("C Z") == (2, 1)
