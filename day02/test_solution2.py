360efeb5272a37d8e05406c5bb36c000ab7632e5
Alexander Grant <alexander.grant@dunelm.com>
2022-12-16 08:23:08 +0000

    Complete algorithm and add solver test.

--- day02/test_solution2.py
+++ day02/test_solution2.py
@@ -1,14 +1,6 @@
-from day02.solution2 import parse_input, calculate_score
-
-
-# def test_calculate():
-#     assert calculate("A Y") == 4
-
-
-# def test_player_move():
-#     assert player_move("A Y") == "X"
-#     assert player_move("B X") == "X"
-#     assert player_move("C Z") == "X"
+from pathlib import Path
+from day02.solution2 import parse_input, calculate_score, solver_2
+from utils.read import read_file_iter
 
 
 def test_parse_input():
@@ -21,3 +13,10 @@ def test_calculate_score():
     assert calculate_score(0, 0) == 4
     assert calculate_score(1, -1) == 1
     assert calculate_score(2, 1) == 7
+
+
+def test_solver():
+    input_path = Path(__file__).parent / "input.txt"
+    input_iter = read_file_iter(input_path)
+
+    assert solver_2(input_iter) == 12
