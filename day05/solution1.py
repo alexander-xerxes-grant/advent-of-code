adc7f7b9b1e58af8027f35782a6be5d30ad089ff
Alexander Grant <alexander.grant@dunelm.com>
2022-12-22 16:32:08 +0000

    Upto parsing instructions.

--- day05/solution1.py
+++ day05/solution1.py
@@ -1,13 +1,60 @@
 from pathlib import Path
 from typing import Iterable
-import numpy as np
-from utils.read import read_file_iter
 
 
-def parse_lines(input):
-    lines = input.strip().split("\n")
-    return lines
+filepath = Path(__file__).parent / "test_input.txt"
 
+
+def parse_input(filepath):
+    with open(filepath) as f:
+        stack_strings, instructions = (
+            i.splitlines() for i in f.read().strip("\n").split("\n\n")
+        )
+    return stack_strings, instructions
+
+
+def create_dict_of_stacks(stacks):
+    stack_dict = {int(digit): [] for digit in stacks[-1].replace(" ", "")}
+    return stack_dict
+
+
+def create_indexes(stacks):
+    indexes = [i for i, char in enumerate(stacks[-1]) if char != " "]
+    return indexes
+
+
+def fill_stacks(stacks, stack_dict):
+    indexes = create_indexes(stacks)
+
+    for string in stacks[:-1]:
+        stack_num = 1
+        for i in indexes:
+            if string[i] != " ":
+                stack_dict[stack_num].insert(0, string[i])
+            stack_num += 1
+    return stack_dict
+
+
+def parse_instructions(instructions):
+    parsed_instructions = []
+    for instruction in instructions:
+        words = instruction.split()
+        crate_num = int(words[1])
+        from_stack = int(words[3])
+        to_stack = int(words[5])
+        parsed_instructions.append((crate_num, from_stack, to_stack))
+    return parsed_instructions
+
+
+# stacks, instructions = parse_stacks_instructions(read_file(filepath))
+# stack_dict = create_dict_of_stacks(stacks)
+# indexes = create_indexes(stacks)
+# # stack_dict = fill_stacks(stacks, stack_dict)``
+
+# print("\nStacks", stacks)
+# print("\nInstructions", instructions)
+# print("\nIndexes", indexes)
+# print("\nStack Dict", stack_dict)
 
 # Use the following line instead if the input is a single line
 # def solver(input: str) -> str:
--- day05/test_solution1.py
+++ day05/test_solution1.py
@@ -1,16 +1,58 @@
-from day05.solution1 import parse_lines
+from pathlib import Path
+
+import pytest
+
+from day05.solution1 import (
+    parse_input,
+    create_dict_of_stacks,
+    create_indexes,
+    fill_stacks,
+    parse_instructions,
+)
 
 
-def test_parse_lines():
-    input_string = """
-    [D]    
-[N] [C]    
-[Z] [M] [P]
- 1   2   3 
-"""
-    assert parse_lines(input_string) == [
-        "[D]    ",
-        "[N] [C]    ",
-        "[Z] [M] [P]",
-        " 1   2   3",
+def test_parse_input():
+    filepath = Path(__file__).parent / "test_input.txt"
+
+    assert parse_input(filepath) == (
+        ["    [D]    ", "[N] [C]    ", "[Z] [M] [P]", " 1   2   3 "],
+        [
+            "move 1 from 2 to 1",
+            "move 3 from 1 to 3",
+            "move 2 from 2 to 1",
+            "move 1 from 1 to 2",
+        ],
+    )
+
+
+def test_create_dict_of_stacks():
+    stacks = ["    [D]    ", "[N] [C]    ", "[Z] [M] [P]", " 1   2   3 "]
+    assert create_dict_of_stacks(stacks) == {1: [], 2: [], 3: []}
+
+
+def test_create_indexes():
+    stacks = ["    [D]    ", "[N] [C]    ", "[Z] [M] [P]", " 1   2   3 "]
+    assert create_indexes(stacks) == [1, 5, 9]
+
+
+def test_fill_stacks():
+    filepath = Path(__file__).parent / "test_input.txt"
+    stacks, _ = parse_input(filepath)
+    stack_dict = {1: [], 2: [], 3: []}
+
+    assert fill_stacks(stacks, stack_dict) == {
+        1: ["Z", "N"],
+        2: ["M", "C", "D"],
+        3: ["P"],
+    }
+
+
+def test_parse_instructions():
+    filepath = Path(__file__).parent / "test_input.txt"
+    _, instructions = parse_input(filepath)
+    assert parse_instructions(instructions) == [
+        (1, 2, 1),
+        (3, 1, 3),
+        (2, 2, 1),
+        (1, 1, 2),
     ]
